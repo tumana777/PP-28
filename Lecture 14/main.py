933264cff012143742f29ec7e78f73951bbc7ec7
tumana777 <tumana991@gmail.com>
2025-06-27 20:05:08 +0400

    add lecture 14

---
+++ Lecture 14/main.py
@@ -0,0 +1,53 @@
+# class Student:
+#     is_active = True
+#     pay = 1000
+#
+# student = Student()
+# student1 = Student()
+#
+# # print(student)
+# # print(student.is_active)
+#
+# student1.is_active = False
+# student1.pay = 2000
+#
+# print(student.is_active, student.pay, sep=", ")
+# print(student1.is_active, student1.pay, sep=", ")
+
+# class Student:
+#     is_active = True
+#     pay = 1000
+#     total_students = 0
+#
+#     def __init__(self, first_name, last_name, age):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.age = age
+#         Student.total_students += 1
+#
+#     def get_email(self):
+#         return f"{self.first_name}_{self.last_name}@gmail.com"
+#
+#
+#
+# fridon = Student("Fridon", "Bagashvili", 25)
+# luka = Student("Luka", "Ckhvedadze", 18)
+# giorgi = Student("Giorgi", "Gabedava", 18)
+
+# print(student)
+# print(student.is_active)
+
+# student1.is_active = False
+# student1.pay = 2000
+
+# print(fridon.first_name)
+#
+# fridon.first_name = "Phridon"
+#
+# print(fridon.first_name)
+
+# print(Student.total_students)
+
+# print(fridon.get_email())
+# print(luka.get_email())
+# print(giorgi.get_email())
