89fcec490fc6c8517448a67d6cb4a87e7ae363fc
tumana777 <tumana991@gmail.com>
2025-09-30 12:44:10 +0400

    change to CBV

--- django_project/store/urls.py
+++ django_project/store/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
-from store.views import index, about, products_json, products, product_detail, add_product, update_product, delete_product
+from store.views import (IndexView, products_json,
+    ProductListView, ProductDetailView, ProductCreateView,
+    ProductUpdateView, ProductDeleteView
+)
+
+from django.views.generic import TemplateView
 
 app_name = 'store'
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('about/', about, name='about'),
+    path('', IndexView.as_view(), name='home'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('products.json/', products_json, name='products_json'),
-    path('products/', products, name='products'),
-    path('products/<int:product_pk>/', product_detail, name='product_detail'),
-    path('add_product/', add_product, name='add_product'),
-    path('update_product/<int:product_pk>/', update_product, name='update_product'),
-    path('delete_product/<int:product_pk>/', delete_product, name='delete_product'),
+    path('products/', ProductListView.as_view(), name='products'),
+    path('products/<int:product_pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('add_product/', ProductCreateView.as_view(), name='add_product'),
+    path('update_product/<int:product_pk>/', ProductUpdateView.as_view(), name='update_product'),
+    path('delete_product/<int:product_pk>/', ProductDeleteView.as_view(), name='delete_product'),
 ]
--- django_project/store/views.py
+++ django_project/store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, JsonResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from django.urls import reverse_lazy
 
 from store.models import Category, Product
 from store.forms import ProductForm
@@ -10,60 +12,99 @@ from store.forms import ProductForm
 # def about(request):
 #     return HttpResponse("<h1>Hello, world. This is about page</h1>")
 
-def index(request):
-    return render(request, 'index.html')
+# def index(request):
+#     return render(request, 'index.html')
 
-def about(request):
-    return render(request, 'about.html')
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+# def about(request):
+#     return render(request, 'about.html')
 
 def products_json(request):
     all_product = Product.objects.all()
     return JsonResponse({'products': list(all_product.values())})
 
-def products(request):
-    all_product = Product.objects.all().select_related('category').order_by('-created_at')
-    return render(request, 'products.html', {'products': all_product})
+# def products(request):
+#     all_product = Product.objects.all().select_related('category').order_by('-created_at')
+#     return render(request, 'products.html', {'products': all_product})
 
-def add_product(request):
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products.html'
+    context_object_name = 'products'
+    queryset = Product.objects.all().select_related('category')
 
-    if request.method == 'POST':
-        form = ProductForm(request.POST)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['count'] = self.queryset.count()
+        context['saxeli'] = 'vaniko'
+        return context
 
-        if form.is_valid():
-            form.save()
-            return redirect('store:products')
-    else:
-        form = ProductForm()
+# def add_product(request):
+#
+#     if request.method == 'POST':
+#         form = ProductForm(request.POST)
+#
+#         if form.is_valid():
+#             form.save()
+#             return redirect('store:products')
+#     else:
+#         form = ProductForm()
+#
+#     return render(request, 'add_product.html', {'form': form})
 
-    return render(request, 'add_product.html', {'form': form})
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'add_product.html'
+    success_url = '/products/'
 
-def update_product(request, product_pk):
-    product = get_object_or_404(Product, pk=product_pk)
+# def update_product(request, product_pk):
+#     product = get_object_or_404(Product, pk=product_pk)
+#
+#     if request.method == 'POST':
+#         form = ProductForm(request.POST, instance=product)
+#
+#         if form.is_valid():
+#             form.save()
+#             return redirect('store:product_detail', product_pk=product_pk)
+#     else:
+#         form = ProductForm(instance=product)
+#
+#     return render(request, 'update_product.html', {'form': form})
 
-    if request.method == 'POST':
-        form = ProductForm(request.POST, instance=product)
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'update_product.html'
+    pk_url_kwarg = 'product_pk'
 
-        if form.is_valid():
-            form.save()
-            return redirect('store:product_detail', product_pk=product_pk)
-    else:
-        form = ProductForm(instance=product)
+    def get_success_url(self):
+        return reverse_lazy('store:product_detail', kwargs={'product_pk': self.object.pk})
 
-    return render(request, 'update_product.html', {'form': form})
+# def delete_product(request, product_pk):
+#     product = get_object_or_404(Product, pk=product_pk)
+#
+#     if request.method == 'POST':
+#         product.delete()
+#         return redirect('store:products')
+#     return redirect('store:product_detail', product_pk=product_pk)
 
-def delete_product(request, product_pk):
-    product = get_object_or_404(Product, pk=product_pk)
+class ProductDeleteView(DeleteView):
+    model = Product
+    pk_url_kwarg = 'product_pk'
+    success_url = '/products/'
 
-    if request.method == 'POST':
-        product.delete()
-        return redirect('store:products')
-    return redirect('store:product_detail', product_pk=product_pk)
+# def product_detail(request, product_pk):
+#     product = get_object_or_404(Product, pk=product_pk)
+#     return render(request, 'product_detail.html', {'product': product})
 
-def product_detail(request, product_pk):
-    product = get_object_or_404(Product, pk=product_pk)
-    return render(request, 'product_detail.html', {'product': product})
-
-
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
+    pk_url_kwarg = 'product_pk'
+    context_object_name = 'product'
 
 
 
